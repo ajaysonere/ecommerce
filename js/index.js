a439688ff3535af2a423a85ffb3e23010e4351b8
ajay sonere <ajaysonere786@gmail.com>
2023-10-13 13:12:18 +0530

    list of item added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,62 @@
-console.log("it is working fine");
+const data = [
+    {
+      id: 1,
+      name: " Amazfit Bip 3 Pro ",
+      img: "https://m.media-amazon.com/images/I/71Cgm+01-PL._AC_UL480_QL65_.jpg",
+      price: 18000,
+      categories: "Luxury",
+    },
+    {
+      id: 2,
+      name: "Apple Watch Series 6",
+      img: "https://m.media-amazon.com/images/I/61nZQ+fv1qL._AC_UL480_QL65_.jpg",
+      price: 17000,
+      categories: "Luxury",
+    },
+    {
+      id: 3,
+      name: "Amazfit Bip 3 Pro",
+      img: "https://m.media-amazon.com/images/I/61GOb969hML._AC_UL480_FMwebp_QL65_.jpg",
+      price: 15000,
+      categories: "Sports",
+    },
+    {
+      id: 4,
+      name: " SAMSUNG Galaxy Watch 6 ",
+      img: "https://m.media-amazon.com/images/I/51x0g9PoieL._AC_UL480_FMwebp_QL65_.jpg",
+      price: 15000,
+      categories: "Sport",
+    },
+    {
+      id: 5,
+      name: "OnePlus Watch ",
+      img: "https://m.media-amazon.com/images/I/51-PGlCDyTS._AC_UL480_FMwebp_QL65_.jpg",
+      price: 7000,
+      categories: "Casual",
+    },
+    {
+        id: 6,
+        name: "Gamrin Unisex-Adult Venu",
+        img: "https://m.media-amazon.com/images/I/81dyZnfwvSL._AC_UL480_FMwebp_QL65_.jpg",
+        price: 5000,
+        categories: "Casual",
+      },
+      {
+        id: 7,
+        name: "Samsung Gear 2 ",
+        img: "https://m.media-amazon.com/images/I/71KG6vlMY5L._AC_UL480_FMwebp_QL65_.jpg",
+        price: 5000,
+        categories: "Casual",
+      },
+      {
+        id: 8,
+        name: "TOZO S2 44MM",
+        img: "https://m.media-amazon.com/images/I/613pEi4kmyL._AC_UL480_FMwebp_QL65_.jpg",
+        price: 7000,
+        categories: "Casual",
+      },
+
+  ];
+
+  const products = document.querySelector('.products');
+  
